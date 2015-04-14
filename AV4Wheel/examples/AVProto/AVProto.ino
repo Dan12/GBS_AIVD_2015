822dee7646aed58d059856f91e6160bdfeaef298
Dan W <dantech88@yahoo.com>
2015-04-14 15:56:31 +0000

    modified avproto code

--- AV4Wheel/examples/AVProto/AVProto.ino
+++ AV4Wheel/examples/AVProto/AVProto.ino
@@ -2,7 +2,7 @@
 #include <Servo.h>
 #include <AV4Wheel.h>
 
-AV4Wheel avpro;
+AV4Wheel avproto;
 
 int buttonPin = 5;
 
@@ -11,15 +11,16 @@ void setup(){
     pinMode(buttonPin,INPUT);
     
     //Parameters: Motor pin a, Encoder pin, Steering Servo pin, Wheel Circumfrenc (in inches)
-    avpro.init(11,7,9, 10.0);
+    avproto.init(11,7,9, 10.0);
 }
 
 void loop(){
     if(digitalRead(buttonPin) == LOW){
         //reverse,duty cycle, servo angle, dist (inches), time (miliseconds) 
-        //Duty cycle: 10-70: stop, 170-90: cw, 200-250: ccw
-        test.move(false, 240, 90, 0, 1000);
-        test.move(false, 240, 65, 0, 1000);
-        test.move(false, 240, 115, 0, 1000);
+        //1 Pin Motor Duty cycle and equivalent directions (slow-fast): 10-70: stop, 170-90: cw, 200-250: ccw
+        avproto.move(false, 230, 90, 0, 1000);
+        avproto.move(false, 230, 65, 0, 1000);
+        avproto.move(false, 230, 90, 0, 1000);
+        avproto.move(false, 230, 115, 0, 1000);
     }
 }
